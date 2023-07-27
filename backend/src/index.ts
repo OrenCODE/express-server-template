77b41899a02041676a85f38bc312426a061115b3
orenCODE <orencodes@gmail.com>
2023-07-27 11:28:53 +0300

    init mongo + prisma client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import router from '@/routers/routes';
+import usersRouter from '@/routers/users.routes';
 import validateEnv from '@/utils/validateEnv';
 
 // App Variables
@@ -23,9 +25,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Routes
+
+app.use('/', router);
+app.use('/', usersRouter);
 
 // Server Activation
 
